f6c8755af0d0adf583f4c13cdcb2066512d0bdb0
samuel <samuel.vaarankorpi@seamk.fi>
2023-12-04 17:30:04 +0200

    first commit

---
+++ gnss_server.py
@@ -0,0 +1,36 @@
+import json
+from flask import Flask, render_template, request, Response
+from flask_cors import CORS, cross_origin # pip install flask-cors
+from flask_socketio import SocketIO, emit
+app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+coordinates = []
+
+@cross_origin()
+@app.route('/')
+def get_all_measurements_map():
+    return render_template('seinajoki.html')
+
+
+@cross_origin()
+@app.route('/api/coordinates')
+def get_all_measurements():
+    return(json.dumps(coordinates))            
+
+@cross_origin()
+@app.route('/uusimittaus', methods = ['POST'])
+def new_measurement():
+    p = request.get_json(force=True)
+    coordinates.append(p)
+    x = json.dumps(p, indent=True)
+    coordinatesjson = json.dumps(coordinates, indent=True)
+    socketio.emit('position', {'data': x})
+    return x
+
+if __name__ == '__main__':
+   socketio.run(app)
+
---
+++ gnsssimu.py
@@ -0,0 +1,19 @@
+
+import json
+import requests
+import time
+ 
+FILENAME = "position.txt"
+ 
+with open(FILENAME, "r") as file:
+    for line in file:
+
+        lat = float(line.split("lat=")[1].split(",")[0])
+        lon = float(line.split("lon=")[1].split(",")[0])
+        obj = [lat, lon]
+        print(lat, lon)
+        response = requests.post("http://localhost:5000/uusimittaus", json = obj)
+        print(response.ok, response.json())
+        time.sleep(0.1)
+
+ 
